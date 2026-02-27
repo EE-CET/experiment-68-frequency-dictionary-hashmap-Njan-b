13c5a622801f80c4f3cee00045d66288037dfe6b
Njan-b <dhrishyadasph2005@gmail.com>
2026-02-27 23:19:56 +0530

    initial commit

--- src/Solution.java
+++ src/Solution.java
@@ -6,17 +6,13 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String line = sc.nextLine();
+        String line = sc.nextLine().trim();
+        String[] words = line.split("\\s+");
 
-        String[] words = line.split(" ");
         HashMap<String, Integer> map = new HashMap<>();
 
         for (String word : words) {
-            if (map.containsKey(word)) {
-                map.put(word, map.get(word) + 1);
-            } else {
-                map.put(word, 1);
-            }
+            map.put(word, map.getOrDefault(word, 0) + 1);
         }
 
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
