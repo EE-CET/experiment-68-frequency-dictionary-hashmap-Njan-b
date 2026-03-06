2b10b94650b7a50d4c7839e11ae041051de9dabf
Dhrishya Das <dhrishyadasph2005@gmail.com>
2026-03-06 09:38:03 +0530

    initial commit

--- src/Solution.java
+++ src/Solution.java
@@ -5,20 +5,16 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
 
-        HashMap<String, Integer> map = new HashMap<>();
+        LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
 
         String[] words = line.split(" ");
 
         for (String word : words) {
-            if (map.containsKey(word)) {
-                map.put(word, map.get(word) + 1);
-            } else {
-                map.put(word, 1);
-            }
+            map.put(word, map.getOrDefault(word, 0) + 1);
         }
 
-        for (String key : map.keySet()) {
-            System.out.println(key + ": " + map.get(key));
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
 }
