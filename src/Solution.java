fe1b8e628e74e7d6861f01c934eff0c550ea1e9b
Dhrishya Das <dhrishyadasph2005@gmail.com>
2026-03-06 09:33:54 +0530

    initial commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,24 +1,24 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        String line = sc.nextLine().trim();
-        String[] words = line.split("\\s+");
+        String line = sc.nextLine();
 
         HashMap<String, Integer> map = new HashMap<>();
 
+        String[] words = line.split(" ");
+
         for (String word : words) {
-            map.put(word, map.getOrDefault(word, 0) + 1);
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
+            }
         }
 
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+        for (String key : map.keySet()) {
+            System.out.println(key + ": " + map.get(key));
         }
-
-        sc.close();
     }
 }
